b7163ecb0f0fa9ed58d468f424a52856146d6684
SilasMeyer4 <meyer-si4@web.de>
2025-03-16 14:40:06 +0100

    testing automatic update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use tauri_plugin_updater::UpdaterExt;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -7,8 +10,31 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                if let Err(e) = update(handle).await {
+                    println!("Update error: {:?}", e);
+                }
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    if let Some(update) = app.updater()?.check().await? {
+        update.download_and_install(
+            |chunk, total| println!("Downloaded {} of {:?}", chunk, total),
+            || println!("Download complete"),
+        ).await?;
+
+        println!("Update installed!");
+        app.restart();
+    }
+    Ok(())
 }
